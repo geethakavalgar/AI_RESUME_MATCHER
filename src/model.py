43338a4e796980dd7cb01c8644c7d667f3a62c99
geethakavalgar <geethachandrashekar29@gmail.com>
2026-04-15 14:24:33 -0500

    Add files via upload

---
+++ src/model.py
@@ -0,0 +1,10 @@
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_similarity(resume, job_desc):
+    emb1 = model.encode(resume)
+    emb2 = model.encode(job_desc)
+    return cosine_similarity([emb1], [emb2])[0][0]
